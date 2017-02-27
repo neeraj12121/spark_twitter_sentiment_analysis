90c8f0f31897bc3b695d1facd125c6970cd0dbbe
Neeraj <ny.neeraj12121@gmail.com>
2017-02-27 23:37:58 +0530

    used jsonpickle module to store tweets

--- tweet_collect.py
+++ tweet_collect.py
@@ -1,12 +1,32 @@
+import tweepy
+import jsonpickle
+
+
 c_key = '......'
 c_secret = '......'
 token_key = '......'
 token_secret = '......'
 
-
 auth = tweepy.OAuthHandler(c_key,c_secret)
 auth.set_access_token(token_key,token_secret)
 
 api = tweepy.API(auth)
+
+totalTweets=1000
+fName='tweets.json'
+tweetCount = 0
+
+with open(fName, 'w') as f:
+    while tweetCount < maxTweets:
+        results = api.search(q='pepsi')
+        if not result:
+            print("No more tweets found")
+            break
+        for tweet in results:
+                f.write(jsonpickle.encode(tweet._json, unpicklable=False) + '\n')
+            tweetCount += len(results)
+            print("Downloaded {0} tweets".format(tweetCount))
+
+print ("Total {0} tweets are downloaded, Saved to {1}".format(tweetCount, fName))
 
 
