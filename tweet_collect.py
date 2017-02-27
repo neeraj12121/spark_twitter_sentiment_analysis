a73dd5d0f71070cc9779f8b05513226b72c8c894
Neeraj <ny.neeraj12121@gmail.com>
2017-02-28 01:57:11 +0530

    updated

--- tweet_collect.py
+++ tweet_collect.py
@@ -17,28 +17,29 @@ totalTweets=1000
 fName='tweets.json'
 tweetCount = 0
 
+
 with open(fName, 'w') as f:
     while tweetCount < maxTweets:
         results = api.search(q='pepsi')
-        if not result:
+        if not results:
             print("No more tweets found")
             break
         for tweet in results:
-                f.write(jsonpickle.encode(tweet._json, unpicklable=False) + '\n')
-            tweetCount += len(results)
-            print("Downloaded {0} tweets".format(tweetCount))
+            f.write(jsonpickle.encode(tweet._json, unpicklable=False) + '\n')
+        tweetCount += len(results)
+        print("Downloaded {0} tweets".format(tweetCount))
 
 print ("Total {0} tweets are downloaded, Saved to {1}".format(tweetCount, fName))
 
 
 class stream(StreamListener):
-    	def on_data(self, data):
-	    fhOut.write(data)
-	    j=json.loads(data)
-	    text=j["text"] 	
-	    print(text)
+    def on_data(self, data):
+	fhOut.write(data)
+	j=json.loads(data)
+	text=j["text"] 	
+	print(text)
 
-	def on_error(self, status):
-	    print("ERROR")
-	    print(status)
+    def on_error(self, status):
+	print("ERROR")
+	print(status)
 
